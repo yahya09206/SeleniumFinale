e8765541ec053198915cba9274c410c200c25463
Yahya <yahyahussein91@gmail.com>
2024-10-15 12:41:00 -0700

    finding all elements that have the letter a in them

--- src/test/java/com/yahya/day02_locators/FindByPartialText_GetText_Method.java
+++ src/test/java/com/yahya/day02_locators/FindByPartialText_GetText_Method.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class FindByPartialText_GetText_Method {
     public static void main(String[] args) {
 
@@ -16,6 +18,16 @@ public class FindByPartialText_GetText_Method {
         // Identify the link using partial link text then get the full text Disappearing Elements
         WebElement addRemove = driver.findElement(By.partialLinkText("Disappearing"));
         System.out.println(addRemove.getText());
+
+        // Provide partial link as the letter "a"
+        // Should return the first one
+        WebElement aLinks = driver.findElement(By.partialLinkText("A"));
+        System.out.println("aLinks.getText() = " + aLinks.getText());
+
+        List<WebElement> allALinks = driver.findElements(By.partialLinkText("A"));
+        for (WebElement allALink : allALinks) {
+            System.out.println(allALink.getText());
+        }
 
         driver.quit();
 
