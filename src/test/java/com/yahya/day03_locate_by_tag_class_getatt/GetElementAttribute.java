22e6f2952f4c40be153b0e2ae8929de0f17ab3bd
Yahya <yahyahussein91@gmail.com>
2024-10-16 09:21:46 -0700

    find by att and tags

---
+++ src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -0,0 +1,16 @@
+package com.yahya.day03_locate_by_tag_class_getatt;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetElementAttribute {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://practice.cydeo.com");
+
+
+    }
+}
