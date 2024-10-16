de8a0b8adf2147f9cca3306e8200bf6f55c47b1c
Yahya <yahyahussein91@gmail.com>
2024-10-16 11:02:42 -0700

    added condition to see if boxes are checked or not

--- src/test/java/com/yahya/day04_common_elements_xpath_css/LocatingCheckBoxes.java
+++ src/test/java/com/yahya/day04_common_elements_xpath_css/LocatingCheckBoxes.java
@@ -16,6 +16,30 @@ public class LocatingCheckBoxes {
         // Identify first checkbox
         WebElement checkbox1 = driver.findElement(By.id("box1"));
         System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
+        checkbox1.getAttribute("checked");
+
+        WebElement checkbox2 = driver.findElement(By.id("box2"));
+        System.out.println("checkbox2.isSelected() = " + checkbox2.isSelected());
+        checkbox2.getAttribute("checked");
+
+        checkbox1.click();
+        System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
+        checkbox2.click();
+        System.out.println("checkbox2.isSelected() = " + checkbox2.isSelected());
+
+        if (!checkbox1.isSelected()){
+            checkbox1.click();
+        } else {
+            System.out.println("Take it easy, already checked");
+        }
+
+        if (!checkbox2.isSelected()){
+            checkbox2.click();
+        } else {
+            System.out.println("Take it easy, already checked");
+        }
+
+
 
         driver.quit();
     }
