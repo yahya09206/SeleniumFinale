bfa8aa673820ba9635208ceb0a563258765e7b40
Yahya <yahyahussein91@gmail.com>
2024-10-18 09:24:41 -0700

    annotations for init and closing browser

--- src/test/java/com/yahya/tests/day05_css_xpath_junit5/YahooSearchPage.java
+++ src/test/java/com/yahya/tests/day05_css_xpath_junit5/YahooSearchPage.java
@@ -1,6 +1,7 @@
 package com.yahya.tests.day05_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,29 +31,28 @@ public class YahooSearchPage {
 
     }
 
+    @AfterEach
+    public void exitDriver(){
+
+        driver.quit();
+    }
+
     @Test
     public void testYahooSearchHomePageTitle(){
 
         String expectedTitle = "Yahoo Search - Web Search";
         Assertions.assertEquals(expectedTitle, driver.getTitle());
-
-        driver.quit();
-
     }
 
     @Test
     public void testYahooSearchResultPageTitle(){
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
         driver.navigate().to("https://search.yahoo.com/");
 
         WebElement searchBox = driver.findElement(By.xpath("//div/input[@id='yschsp']"));
         searchBox.sendKeys("Selenium", Keys.ENTER);
 
         Assertions.assertTrue(driver.getTitle().startsWith("Selenium"));
-
-        driver.quit();
-
     }
 }
