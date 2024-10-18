19641a9884cf9ef19076c9ce147dc50b09138ff2
Yahya <yahyahussein91@gmail.com>
2024-10-18 09:07:30 -0700

    adding assert true

--- src/test/java/com/yahya/tests/day05_css_xpath_junit5/FirstTest.java
+++ src/test/java/com/yahya/tests/day05_css_xpath_junit5/FirstTest.java
@@ -11,6 +11,17 @@ public class FirstTest {
         int num1 = 10;
         int num2 = 7;
         int expectedResult = 17;
-        Assertions.assertEquals(expectedResult, 10 + 7);
+        Assertions.assertEquals(expectedResult, num1 + num2);
+    }
+
+    @Test
+    public void testString(){
+
+        String msg = "I love selenium";
+        // Assert that the message equals "I love selenium"
+        Assertions.assertEquals("I love selenium", msg);
+
+        // Assert this message starts with "I love"
+        Assertions.assertTrue(msg.startsWith("I love"));
     }
 }
