d9a904f685f5186ccaef98ca2140cd70314bdb8d
Yahya <yahyahussein91@gmail.com>
2024-10-18 09:18:49 -0700

    verified title after search

--- src/test/java/com/yahya/tests/day05_css_xpath_junit5/YahooSearchPage.java
+++ src/test/java/com/yahya/tests/day05_css_xpath_junit5/YahooSearchPage.java
@@ -3,7 +3,10 @@ package com.yahya.tests.day05_css_xpath_junit5;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YahooSearchPage {
@@ -27,5 +30,21 @@ public class YahooSearchPage {
         Assertions.assertEquals(expectedTitle, driver.getTitle());
 
         driver.quit();
+    }
+
+    @Test
+    public void testYahooSearchResultPageTitle(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://search.yahoo.com/");
+
+        WebElement searchBox = driver.findElement(By.xpath("//div/input[@id='yschsp']"));
+        searchBox.sendKeys("Selenium", Keys.ENTER);
+
+        Assertions.assertTrue(driver.getTitle().startsWith("Selenium"));
+
+        driver.quit();
+
     }
 }
