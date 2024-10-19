5f810a8b9455089d4fe201d3295e85a5ce662887
Yahya <yahyahussein91@gmail.com>
2024-10-19 01:49:58 -0700

    using switchto method to track alert type

---
+++ src/test/java/com/yahya/tests/day06_junit_utility_methods/AlertTests.java
@@ -0,0 +1,33 @@
+package com.yahya.tests.day06_junit_utility_methods;
+
+import com.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class AlertTests extends TestBase {
+
+    /**
+     * regular(ok0
+     * confirmation(ok and cancel)
+     * prompt(enter text)
+     */
+    @Test
+    public void testJavaScriptAlert(){
+
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+
+        // Alert interface from Selenium is used to deal with alerts
+        // your driver object can switchTo the alert and take some action on it
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+
+        // the driver first needs to switch to the alert box since it is not part of the html
+        // using switchTo() method followed by alert() method
+        // .alert() methods returns a type of Alert from Selenium
+        // it has 4 options
+            // accept() --> click ok button
+            // dismiss() --> click on cancel button
+            // sendKey(yourText) --> enter text into prompts
+        driver.switchTo().alert().accept();
+        System.out.println("The End!!");
+    }
+}
--- src/test/java/com/yahya/utility/TestBase.java
+++ src/test/java/com/yahya/utility/TestBase.java
@@ -21,6 +21,9 @@ public abstract class TestBase {
 //        WebDriverManager.chromedriver().setup();
 //        driver = new ChromeDriver();
         driver = WebDriverFactory.getDriver("chrome");
+        // Instruct driver to wait maximum ammount of time before throwing NoSuchElement Exception by using implicit wait
+        // if element is not available, it will wait for a maximum of 10 seconds
+        // if element is found before 10s it will move on to next step
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
