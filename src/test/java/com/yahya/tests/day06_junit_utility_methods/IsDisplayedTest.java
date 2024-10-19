18b9415bc4beaef0b8b8379a08adf36aff7371e8
Yahya <yahyahussein91@gmail.com>
2024-10-19 01:00:37 -0700

    checking for error message before and after attempt to login

---
+++ src/test/java/com/yahya/tests/day06_junit_utility_methods/IsDisplayedTest.java
@@ -0,0 +1,47 @@
+package com.yahya.tests.day06_junit_utility_methods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IsDisplayedTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void yahooPage(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+        driver.quit();
+    }
+
+    // navigate to form page
+    // identify the error message exists but is not displayed
+    // enter text 'a' and check the error message is displayed
+    @Test
+    public void testErrorMsg(){
+
+        driver.navigate().to("https://practice.cydeo.com/registration_form");
+        String xpathForErrorMsg = "//small[.='first name must be more than 2 and less than 64 characters long']";
+        WebElement stringErrorElement = driver.findElement(By.xpath(xpathForErrorMsg));
+
+        System.out.println("stringErrorElement.isDisplayed() = " + stringErrorElement.isDisplayed());
+
+        WebElement firstNameField = driver.findElement(By.name("firstname"));
+        firstNameField.sendKeys("a");
+
+        System.out.println("stringErrorElement.isDisplayed() = " + stringErrorElement.isDisplayed());
+    }
+}
