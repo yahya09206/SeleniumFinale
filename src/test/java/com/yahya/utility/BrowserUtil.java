e2071a8326392004c48f43bdfd56a6128014ee2e
Yahya <yahyahussein91@gmail.com>
2024-10-19 10:44:06 -0700

    static method for waiting

---
+++ src/test/java/com/yahya/utility/BrowserUtil.java
@@ -0,0 +1,13 @@
+package com.yahya.utility;
+
+public class BrowserUtil {
+
+    public static void waitFor(int seconds){
+
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
