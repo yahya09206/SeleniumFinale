9eebc81019bf251f4e5ec97e5330eac6bac4e539
Yahya <yahyahussein91@gmail.com>
2024-10-19 01:21:17 -0700

    set up webdriverfactory to get drivr automatically

---
+++ src/test/java/com/yahya/utility/WebDriverFactory.java
@@ -0,0 +1,32 @@
+package com.yahya.utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserName){
+
+        WebDriver driver;
+
+        switch (browserName.toLowerCase()){
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                 break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = null;
+                System.out.println("UNKNOWN BROWSER TYPE " + browserName);;
+        }
+
+        return driver;
+    }
+}
