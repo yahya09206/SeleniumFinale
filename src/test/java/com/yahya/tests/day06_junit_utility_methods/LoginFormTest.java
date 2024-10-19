2199043bb0ee28eaa5a71c3a3e8aa96f14c8d667
Yahya <yahyahussein91@gmail.com>
2024-10-19 00:35:03 -0700

    added starter code

--- src/test/java/com/yahya/tests/day06_junit_utility_methods/LoginFormTest.java
+++ src/test/java/com/yahya/tests/day06_junit_utility_methods/LoginFormTest.java
@@ -1,6 +1,28 @@
 package com.yahya.tests.day06_junit_utility_methods;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginFormTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void yahooPage(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+        driver.quit();
+    }
 
 
 }
