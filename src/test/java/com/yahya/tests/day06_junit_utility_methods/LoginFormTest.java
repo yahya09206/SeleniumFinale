bbc15648190b565913de898b8978a9575a5132f6
Yahya <yahyahussein91@gmail.com>
2024-10-19 00:36:25 -0700

    xpath for getting header

--- src/test/java/com/yahya/tests/day06_junit_utility_methods/LoginFormTest.java
+++ src/test/java/com/yahya/tests/day06_junit_utility_methods/LoginFormTest.java
@@ -3,7 +3,10 @@ package com.yahya.tests.day06_junit_utility_methods;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginFormTest {
@@ -22,6 +25,15 @@ public class LoginFormTest {
     public void tearDown(){
 
         driver.quit();
+    }
+
+    @Test
+    public void testLogin(){
+
+        driver.get("http://practice.cydeo.com/login");
+
+        WebElement header = driver.findElement(By.xpath("//h2[.='Login Page'"));
+        System.out.println("header.getText() = " + header.getText());
     }
 
 
