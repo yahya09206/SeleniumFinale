80984b9abaa7e96906b12c79027456f7ddb1e015
Yahya <yahyahussein91@gmail.com>
2024-10-19 01:03:33 -0700

    added assertions instead of just printing

--- src/test/java/com/yahya/tests/day06_junit_utility_methods/IsDisplayedTest.java
+++ src/test/java/com/yahya/tests/day06_junit_utility_methods/IsDisplayedTest.java
@@ -2,6 +2,7 @@ package com.yahya.tests.day06_junit_utility_methods;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -35,13 +36,17 @@ public class IsDisplayedTest {
 
         driver.navigate().to("https://practice.cydeo.com/registration_form");
         String xpathForErrorMsg = "//small[.='first name must be more than 2 and less than 64 characters long']";
-        WebElement stringErrorElement = driver.findElement(By.xpath(xpathForErrorMsg));
 
+        // Error message will only be displayed after user attempts to login with 2 or less characters
+        WebElement stringErrorElement = driver.findElement(By.xpath(xpathForErrorMsg));
         System.out.println("stringErrorElement.isDisplayed() = " + stringErrorElement.isDisplayed());
+        Assertions.assertFalse(stringErrorElement.isDisplayed());
 
         WebElement firstNameField = driver.findElement(By.name("firstname"));
         firstNameField.sendKeys("a");
 
         System.out.println("stringErrorElement.isDisplayed() = " + stringErrorElement.isDisplayed());
+        Assertions.assertTrue(stringErrorElement.isDisplayed());
+
     }
 }
