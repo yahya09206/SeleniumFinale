19128e3cc4f025a7ebe79a080a2c04194d34597e
Yahya <yahyahussein91@gmail.com>
2024-10-20 00:52:26 -0700

    implemented web driver wait

--- src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/ExplicitWaitTest.java
+++ src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/ExplicitWaitTest.java
@@ -3,6 +3,11 @@ package com.yahya.tests.day09_explicitwait_singleton_properties;
 import com.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ExplicitWaitTest extends TestBase {
 
@@ -16,6 +21,17 @@ public class ExplicitWaitTest extends TestBase {
 
         //3. The title will show loading.. until a certain time
         // wait until the title value becomes "Dynamic Title"
+        // this is how you create WebDriverWait object with driver and a max time to wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+        // then we use the until method that accepts expected conditions
+        // ExpectedConditions helper class has a lot of pre-written conditions
+        // for common scenarios, so we don't have to build our own
+        // in this case we are checking for title equals dynamic title
+        wait.until(ExpectedConditions.titleIs("Dynamic Title"));
+
+        System.out.println("THE END!");
+
+
 
     }
 }
