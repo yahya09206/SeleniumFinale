60191e8aa52195d47bb43cd2730bc46e4467efbc
Yahya <yahyahussein91@gmail.com>
2024-10-20 01:32:26 -0700

    created method for logging in with webdriver

--- src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/WebOrderTest.java
+++ src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/WebOrderTest.java
@@ -1,6 +1,8 @@
 package com.yahya.tests.day09_explicitwait_singleton_properties;
 
+import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.TestBase;
+import com.yahya.utility.WebOrderUtility;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -10,9 +12,13 @@ public class WebOrderTest extends TestBase {
     public void testLogin(){
 
         driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
-        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+//        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+//        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+//        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+
+        WebOrderUtility.login(driver);
+
+        BrowserUtil.waitFor(4);
 
 
     }
