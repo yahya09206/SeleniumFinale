1b6d1abe3aeb3250a364675e88b75e97026d0a16
Yahya <yahyahussein91@gmail.com>
2024-10-20 08:57:33 -0700

    set up driver class with single driver instance

--- src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/SingletonTest.java
+++ src/test/java/com/yahya/tests/day09_explicitwait_singleton_properties/SingletonTest.java
@@ -1,8 +1,18 @@
 package com.yahya.tests.day09_explicitwait_singleton_properties;
 
+import com.yahya.utility.Driver;
+import com.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
 
-public class SingletonTest {
+public class SingletonTest extends TestBase {
+
+    @Test
+    public void testDriverUtilClass(){
+
+        Driver.getDriver();
+        Driver.getDriver();
+        Driver.getDriver();
+    }
 
     @Test
     public void testSingleton(){
--- src/test/java/com/yahya/utility/Driver.java
+++ src/test/java/com/yahya/utility/Driver.java
@@ -1,4 +1,31 @@
 package com.yahya.utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * A class with a method that will only return a Single object no matter how many times you ask for it
+ * Technique used from singleton pattern
+ */
 public class Driver {
+
+    private static WebDriver obj;
+
+    private Driver(){
+
+    }
+
+    public static WebDriver getDriver(){
+
+        if (obj == null){
+            WebDriverManager.chromedriver().setup();
+            obj = new ChromeDriver();
+            System.out.println("One and only instance");
+            return obj;
+        } else {
+            System.out.println("Instance already exists");
+            return obj;
+        }
+    }
 }
