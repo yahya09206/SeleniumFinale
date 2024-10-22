b02d454cabd2bf906bdc7560827a0486c8071fe1
Yahya <yahyahussein91@gmail.com>
2024-10-22 09:50:06 -0700

    new day

--- src/test/java/com/yahya/tests/day10_driver_method/WebOrderPracticeTest.java
+++ src/test/java/com/yahya/tests/day10_driver_method/WebOrderPracticeTest.java
@@ -1,6 +1,7 @@
 package com.yahya.tests.day10_driver_method;
 
 import com.yahya.utility.TestBase;
+import com.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
 
 public class WebOrderPracticeTest extends TestBase {
@@ -8,6 +9,6 @@ public class WebOrderPracticeTest extends TestBase {
     @Test
     public void testDriverPractice(){
 
-
+        WebOrderUtil.openWebOrderApp();
     }
 }
