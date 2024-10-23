e0d7561a3225d62a22af3b29da82f5a4ce630d63
Yahya <yahyahussein91@gmail.com>
2024-10-23 12:50:14 -0700

    reading value from config file using get property method

--- src/test/java/com/yahya/tests/day10_driver_method/PropertyFileReadTest.java
+++ src/test/java/com/yahya/tests/day10_driver_method/PropertyFileReadTest.java
@@ -3,17 +3,27 @@ package com.yahya.tests.day10_driver_method;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PropertyFileReadTest {
 
     @Test
-    public void testInitialRead() throws FileNotFoundException {
+    public void testInitialRead() throws IOException {
 
         // Open connection to file using FileInputStream
         FileInputStream fileInputStream = new FileInputStream("config.properties");
 
+        // Create empty Properties object
+        Properties myProperty = new Properties();
+        // Load the fileInputStream into the Properties object
+        myProperty.load(fileInputStream);
+        // close the connection by calling close method of FileInputStream
+        fileInputStream.close();
 
+        // Read from properties using key
+        String helloValue = myProperty.getProperty("hello");
+        System.out.println("helloValue = " + helloValue);
 
 
     }
