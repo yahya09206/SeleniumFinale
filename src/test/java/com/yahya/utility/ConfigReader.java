4446271d0d7e53526dc1567101da383e0967d2df
Yahya <yahyahussein91@gmail.com>
2024-10-23 13:05:26 -0700

    set up config read class

---
+++ src/test/java/com/yahya/utility/ConfigReader.java
@@ -0,0 +1,29 @@
+package com.yahya.utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    //Declare Properties object at class level so it can be accessible in static method
+    private static Properties properties =  new Properties();
+
+    static {
+        try {
+            FileInputStream inputStream = new FileInputStream("config.properties");
+            properties.load(inputStream);
+            inputStream.close();
+        } catch (IOException e ){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Read from the file
+     */
+    public static String read(String key){
+
+        return properties.getProperty(key);
+    }
+}
