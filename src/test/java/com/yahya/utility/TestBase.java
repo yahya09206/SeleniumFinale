b2946ae2a15e50ca1173196c713cc7d0454ccfd2
Yahya <yahyahussein91@gmail.com>
2024-10-23 12:11:32 -0700

    fixed error where driver wasnt set up correctly in testbase class

--- src/test/java/com/yahya/utility/TestBase.java
+++ src/test/java/com/yahya/utility/TestBase.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static com.yahya.utility.Driver.getDriver;
@@ -22,11 +23,11 @@ public abstract class TestBase {
 
 //        WebDriverManager.chromedriver().setup();
 //        driver = new ChromeDriver();
-        driver = WebDriverFactory.getDriver("chrome");
-        // Instruct driver to wait maximum ammount of time before throwing NoSuchElement Exception by using implicit wait
+        driver = Driver.getDriver();
+        // Instruct driver to wait maximum amount of time before throwing NoSuchElement Exception by using implicit wait
         // if element is not available, it will wait for a maximum of 10 seconds
         // if element is found before 10s it will move on to next step
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
 
