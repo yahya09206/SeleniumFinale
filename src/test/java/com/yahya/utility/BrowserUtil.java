d81dc722601d4c1e5e5900fb39b0ee2a424a7bb0
Yahya <yahyahussein91@gmail.com>
2024-10-23 12:20:36 -0700

    new method for checking visibility of element

--- src/test/java/com/yahya/utility/BrowserUtil.java
+++ src/test/java/com/yahya/utility/BrowserUtil.java
@@ -1,13 +1,40 @@
 package com.yahya.utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BrowserUtil {
 
-    public static void waitFor(int seconds){
+//    public static void waitFor(int seconds){
+//
+//        try {
+//            Thread.sleep(seconds * 1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
+    /**
+     * This method will check for visibility of element within the time given
+     * @param locator By.id or By.xpath or By. Whatever
+     * @param timeToWait in time to wait
+     * @return true if the element is found within the time and given, false if not
+     */
+    public static boolean checkVisibilityOfElement(By locator, int timeToWait){
+
+        boolean result = false;
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWait));
         try {
-            Thread.sleep(seconds * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_MainContent_status")));
+        } catch (TimeoutException e) {
+            System.out.println("The error message is " + e.getMessage());
         }
+
     }
 }
